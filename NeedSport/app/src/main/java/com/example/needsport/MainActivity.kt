351ede6ccca498e9cf336ec2b429d246fa8e2d77
fernando123241 <66262381+fernando123241@users.noreply.github.com>
2022-05-21 20:12:34 +0700

    Nambah Intent ke CallCenter

--- NeedSport/app/src/main/java/com/example/needsport/MainActivity.kt
+++ NeedSport/app/src/main/java/com/example/needsport/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.needsport
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val callCenterBtn: Button = findViewById (R.id.btn_callCenter)
+
+        callCenterBtn.setOnClickListener() {
+            val intent = Intent(this, CallCenter::class.java)
+            startActivity(intent)
+        }
+
+        }
     }
-}
